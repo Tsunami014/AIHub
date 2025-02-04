c02fbeb5ed75f2687a9d9a606df4d191a232c9b5
Tsunami014 (Max) <Tsunami014@duck.com>
2025-02-04 11:53:20 +1100

    Added a sidebar button and a 'template website'

--- AIHub/__main__.py
+++ AIHub/__main__.py
@@ -2,12 +2,19 @@ import flask
 import os
 
 PATH = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(PATH, 'UI', 'template.html')) as f:
+    TEMPLATE = f.read()
+
+def apply(website):
+    with open(os.path.join(PATH, 'UI', website)) as f:
+        fc = f.read()
+    return TEMPLATE.replace('<!-- INSERT CODE -->', fc)
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    return open(os.path.join(PATH, 'UI', 'index.html')).read()
+    return apply('init.html')
 
 if __name__ == '__main__':
     app.run()
